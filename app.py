83a949b97af8f38225c1f2576f4fd56cbf70fa99
amit1809 <amitsharma1809@yahoo.com>
2020-04-24 17:38:01 -0700

    Add support for scantron.json

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, escape, request, jsonify, g
 import os
-from scan_scantron import scan_file
+from scan_scantron import scan_jpeg_file, scan_json_file
 from handle_db import setup_db, close_connection, commit_db, get_db
 import handle_db
 import json
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
 
 #location where scantrons will be saved
-UPLOAD_LOCATION = os.getcwd()
+UPLOAD_LOCATION = os.getcwd()+"/files/"
 
 #initial test and scantron ids
 test_id = 1
@@ -17,7 +17,7 @@ scantron_id = 1
 
 @app.route('/')
 def welcome():
-    return "Add test and scantron"
+    return "Add test details and scantron"
 
 @app.before_first_request
 def setup():
@@ -44,47 +44,25 @@ def add_test():
     print(last_inserted)
     return jsonify(last_inserted), 201
 
-@app.route('/api/tests/temp/scantrons', methods=['POST'])
-def upload_scantrons_details():
-    global scantron_id
-    if not request.is_json:
-        return "Please pass JSON object for test details"
-    content = request.get_json()
-    scantron_url = content["scantron_url"]
-    name = content["name"]
-    subject_name = content["subject"]
-    answers = content["scanned_answers"]
-    scantron_answers_list = list(answers.values())
-    scantron_answers_str = ','.join(scantron_answers_list)
-    cur = get_db().cursor()
-
-    #get test_id and answers key for subject name
-    cur.execute("SELECT test_id FROM test_details WHERE subject=?", (subject_name,))
-    test_id = str(cur.fetchone()[0])
-    cur.execute("SELECT answer_keys FROM test_details WHERE subject=?", (subject_name,))
-    answer_key = str(cur.fetchone()[0])
-
-    #get score by comparing answer_key and scantron_answers
-    answer_key_list = list(answer_key.split(","))
-
-    # Verify answers and calculate score
-    score = get_score(answer_key_list, scantron_answers_list)
-
-    # insert scantron details in DB
-    handle_db.insert_scantron(scantron_id, scantron_url, name, subject_name, score,scantron_answers_str, test_id)
-
-    last_inserted = handle_db.query_scantron(scantron_id)
-    scantron_id += 1
-    close_connection()
-    return jsonify(last_inserted), 201
-
 @app.route('/api/tests/<int:file_id>/scantrons', methods=['POST'])
-def upload_scantrons(file_id):
+def upload_scantrons_json(file_id):
+    global scantron_id
+    FILE_PATH = os.path.join(UPLOAD_LOCATION, str(file_id) + ".json")
+    with open(FILE_PATH, "wb") as fp:
+        fp.write(request.data)
+    file_contents = scan_json_file(FILE_PATH)
+    print(file_contents)
+    insert_data = handle_scantron(file_contents, FILE_PATH)
+
+    return jsonify(insert_data), 201
+
+@app.route('/api/tests/<int:file_id>/scantrons_jpeg', methods=['POST'])
+def upload_scantrons_jpeg(file_id):
     global scantron_id
     FILE_PATH = os.path.join(UPLOAD_LOCATION, str(file_id)+".jpeg")
     with open(FILE_PATH, "wb") as fp:
         fp.write(request.data)
-    file_contents = scan_file(FILE_PATH)
+    file_contents = scan_jpeg_file(FILE_PATH)
     print(file_contents)
     #scantron_id += 1
     return "Scantron uploaded", 201
@@ -103,6 +81,38 @@ def get_all_tests():
 def get_all_scantrons():
     result = handle_db.query_scantron(None)
     return jsonify(result), 201
+
+def handle_scantron(content, file_path):
+    global scantron_id
+    print(file_path)
+    #scantron_url = content["scantron_url"]
+    scantron_url = "http://localhost:5000/files/"+ file_path[file_path.index("/files/") + len("/files/"):]
+    name = content["name"]
+    subject_name = content["subject"]
+    answers = content["scanned_answers"]
+    scantron_answers_list = list(answers.values())
+    scantron_answers_str = ','.join(scantron_answers_list)
+    cur = get_db().cursor()
+
+    # get test_id and answers key for subject name
+    cur.execute("SELECT test_id FROM test_details WHERE subject=?", (subject_name,))
+    test_id = str(cur.fetchone()[0])
+    cur.execute("SELECT answer_keys FROM test_details WHERE subject=?", (subject_name,))
+    answer_key = str(cur.fetchone()[0])
+
+    # get score by comparing answer_key and scantron_answers
+    answer_key_list = list(answer_key.split(","))
+
+    # Verify answers and calculate score
+    score = get_score(answer_key_list, scantron_answers_list)
+
+    # insert scantron details in DB
+    handle_db.insert_scantron(scantron_id, scantron_url, name, subject_name, score, scantron_answers_str, test_id)
+
+    last_inserted = handle_db.query_scantron(scantron_id)
+    scantron_id += 1
+    close_connection()
+    return last_inserted
 
 def get_score(answer_key, scantron_key):
     print(answer_key)
--- scan_scantron.py
+++ scan_scantron.py
@@ -1,9 +1,14 @@
 #import image
+import json
 from PIL import Image
-import pytesseract
+#import pytesseract
 
-def scan_file(file):
+def scan_jpeg_file(file):
     text = pytesseract.image_to_string(Image.open(file))
     return text
 
+def scan_json_file(file):
+    with open(file, "r") as fp:
+        scantron_dict = json.load(fp)
+    return scantron_dict
 #print(scan_file('scantron-100.jpg'))
